8da83d4eecc2cd34e06f07e80cebb45ce355b828
hpmcwill <hamish.mcwilliam@bioinfo-user.org.uk>
2016-07-03 18:10:41 +0100

    Wirte file summary data to a SQLite database. Add meta-data extraction for common meta-data fields using the Image::ExifTool library to access meta-data from common file formats.

--- file_report.pl
+++ file_report.pl
@@ -32,15 +32,16 @@ use warnings;
 use strict;
 
 # Load modules
-use Digest;             # Message digest or checksum calculation
-use Getopt::Long;       # Command-line argument handling
-use File::Basename;     # File name processing.
-use File::Find;         # 'find' utility functionallity
+use Digest;                       # Message digest/checksum calculation
+use Getopt::Long;                 # Command-line argument handling
+use File::Basename;               # File name processing.
+use File::Find;                   # 'find' utility functionallity
 #use Time::HiRes qw(stat lstat);
-use File::stat;         # 'stat' function object interface
-use POSIX qw(strftime); # Time/date formatting
-use DBI;                # Database interface.
-use Data::Dumper;       # Debug output
+use File::stat;                   # 'stat' function object interface
+use POSIX qw(strftime);           # Time/date formatting
+use DBI;                          # Database interface.
+use Image::ExifTool qw(:Public);  # Metadata access
+use Data::Dumper;                 # Debug output
 
 
 # Find and load method to get MIME type for a file.
@@ -199,7 +200,6 @@ sub open_db {
 			    AutoCommit => 1,
 			});
     $dbh->sqlite_busy_timeout(60000); # 1 min.
-    #$dbh->do('BEGIN IMMEDIATE TRANSACTION');
     $dbh->do("PRAGMA foreign_keys = ON");
     &print_debug_message('open_db', 'End', 1);
     return $dbh;
@@ -216,10 +216,6 @@ Close the database.
 sub close_db {
     &print_debug_message('close_db', 'Begin', 1);
     my $dbh = shift;
-    #if(! $dbh->{ReadOnly}) {
-	#&print_debug_message('close_db', 'Commit changes', 1);
-	#$dbh->commit();
-    #}
     $dbh->disconnect();
     &print_debug_message('close_db', 'End', 1);
 }
@@ -236,16 +232,30 @@ sub init_db {
     &print_debug_message('init_db', 'Begin', 1);
     # If old database exists... delete it.
     if(-f $DBFILENAME) {
+	&print_debug_message('init_db', 'Delete old database', 1);
 	unlink($DBFILENAME);
     }
     # Open database, and thus create it.
     my $dbh =  &open_db();
     # Create table(s).
     $dbh->do(q{
+CREATE TABLE source(
+  sourceId INTEGER PRIMARY KEY AUTOINCREMENT,
+  Make TEXT,
+  Model TEXT,
+  Software TEXT,
+  FileSource TEXT,
+  SerialNumber TEXT
+);
+});
+    if($dbh->err) {
+	print STDERR $dbh->errstr, "\n";
+    }
+    $dbh->do(q{
 CREATE TABLE files(
   fullFilename TEXT PRIMARY KEY,
   mtime TEXT,
-  sizeKb INT,
+  sizeBytes INT,
   sizeBlocks INT,
   blockSize INT,
   MD5 TEXT NOT NULL,
@@ -254,7 +264,13 @@ CREATE TABLE files(
   dirName TEXT,
   fileName TEXT,
   fileExtension TEXT,
-  fileType TEXT
+  fileType TEXT,
+  Author TEXT,
+  Title TEXT,
+  Comment TEXT,
+  Copyright TEXT,
+  sourceId INTEGER,
+  FOREIGN KEY(sourceId) REFERENCES source(sourceId)
 );
 });
     if($dbh->err) {
@@ -265,42 +281,104 @@ CREATE TABLE files(
     &print_debug_message('init_db', 'End', 1);
 }
 
+=head2 prepare_row_data()
+
+Prepare row data for addition to database.
+
+  ($names, $qTemplate) = &prepare_row_data($colValues_hash);
+
+=cut
+
+sub prepare_row_data {
+    &print_debug_message('prepare_row_data', 'Begin', 21);
+    my $colValues = shift;
+    &print_debug_message('prepare_row_data', 'colValues: ' . Dumper($colValues), 22);
+    my $names = '';
+    my $qTemplate = '';
+    my (@colNameList) = keys(%$colValues);
+    foreach my $colName (@colNameList) {
+	$names .= $colName . ',';
+	$qTemplate .= '?,';
+    }
+    $names =~ s/,$//;
+    $qTemplate =~ s/,$//;
+    &print_debug_message('prepare_row_data', 'End', 21);
+    return ($names, $qTemplate);
+}
+
+=head2 add_source()
+
+Add/update database table C<source>.
+
+  $source_id = &add_source($dbh, $colValues_hash);
+
+=cut
+
+sub add_source {
+    &print_debug_message('add_source', 'Begin', 11);
+    my $dbh = shift;
+    my $colValues = shift;
+    my $source_id;
+    &print_debug_message('add_source', 'colValues: ' . Dumper($colValues), 2);
+    # Look for existing entry.
+    my $w_clause = '';
+    foreach my $keyName (keys(%$colValues)) {
+	$w_clause .= ' AND' if($w_clause ne '');
+	$w_clause .= " $keyName IS ?"
+    }
+    my $sth0 = $dbh->prepare(qq{
+SELECT sourceId FROM source WHERE $w_clause;
+});
+    $sth0->execute(values(%$colValues)) or die $dbh->errstr;
+    # Found entry(s)...
+    my $row_count = 0;
+    while( my $row_hash = $sth0->fetchrow_hashref() ) {
+	$source_id = $row_hash->{'sourceId'};
+	$row_count++;
+    }
+    &print_debug_message('add_source', "row_count: $row_count", 13);
+    # No entry... add new entry.
+    unless($row_count) {
+	my ($names, $qTemplate) = &prepare_row_data($colValues);
+	&print_debug_message('add_source', 'names: ' . Dumper($names), 13);
+	# Insert new row.
+	my $tmpSth = $dbh->prepare(qq{
+INSERT OR REPLACE INTO source($names) VALUES ($qTemplate);
+	});
+	$tmpSth->execute(values(%$colValues)) or die $dbh->errstr;
+	# Get the source Id for the new entry.
+	$sth0->execute(values(%$colValues)) or die $dbh->errstr;
+	# Found entry(s)...
+	while( my $row_hash = $sth0->fetchrow_hashref() ) {
+	    $source_id = $row_hash->{'sourceId'};
+	}
+    }
+    &print_debug_message('add_source', "source_id: $source_id", 11);
+    &print_debug_message('add_source', 'End', 11);
+    return $source_id;
+}
+
 =head2 add_files()
 
 Add/update database table C<files>.
 
-  &add_files($dbh, %colValues);
+  &add_files($dbh, $colValues_hash);
 
 =cut
 
 sub add_files {
-    &print_debug_message('add_files', 'Begin', 1);
-    # List of columns to insert
-    my $colNames = '';
-    my $values = '';
+    &print_debug_message('add_files', 'Begin', 11);
     my $dbh = shift;
-    my (%colValues) = @_;
-    &print_debug_message('add_files', 'colValues: ' . Dumper(\%colValues), 2);
-    my (@colNameList) = keys(%colValues);
-    foreach my $colName (@colNameList) {
-	$colNames .= $colName . ',';
-	if($colValues{$colName} ne '') {
-	    my $tmpValues = $colValues{$colName};
-	    $tmpValues =~ s/'/''/g;
-	    $values .= "'" . $tmpValues . "',";
-	}
-	else {
-	    $values .= "'',"
-	}
-    }
-    $colNames =~ s/,$//;
-    $values =~ s/,$//;
+    my $colValues = shift;
+    &print_debug_message('add_files', 'colValues: ' . Dumper($colValues), 2);
+    my ($names, $qTemplate) = &prepare_row_data($colValues);
+    &print_debug_message('add_files', 'names: ' . Dumper($names), 13);
     # Insert new row.
     my $tmpSth = $dbh->prepare(qq{
-INSERT OR REPLACE INTO files($colNames) VALUES ($values);
+INSERT OR REPLACE INTO files($names) VALUES ($qTemplate);
 });
-    $tmpSth->execute() or die $dbh->errstr;
-    &print_debug_message('add_files', 'End', 1);
+    $tmpSth->execute(values(%$colValues)) or die $dbh->errstr;
+    &print_debug_message('add_files', 'End', 11);
 }
 
 =head2 checksums()
@@ -313,6 +391,7 @@ names are those used by the L<Digest> module.
 =cut
 
 sub checksums {
+    &print_debug_message('checksums', 'Begin', 11);
     my $digest_method = shift;
     my $filename = shift;
     my $rel_filename = shift;
@@ -336,6 +415,7 @@ sub checksums {
 	    $checksums{$d_method} = $digest->hexdigest();
 	}
     }
+    &print_debug_message('checksums', 'End', 11);
     return %checksums;
 }
 
@@ -368,6 +448,7 @@ value "unknown" returned.
 =cut
 
 sub file_mime_type {
+    &print_debug_message('file_mime_type', 'Begin', 11);
     my $mime_method = shift;
     my $filename = shift;
     my $esc_filename = shift;
@@ -404,6 +485,7 @@ sub file_mime_type {
     else {
 	warn "Invalid MIME method spectified: $mime_method";
     }
+    &print_debug_message('file_mime_type', 'End', 11);
     return $mime_type;
 }
 
@@ -416,6 +498,7 @@ Get file information using the UNIX L<file(1)> utility.
 =cut
    
 sub file_info_str {
+    &print_debug_message('file_info_str', 'Begin', 11);
     my $esc_filename = shift;
     my $rel_filename = shift;
     my $file_utility = shift;
@@ -428,8 +511,84 @@ sub file_info_str {
     chomp($file_type_str);
     $file_type_str =~ s/\s+/ /g;
     $file_type_str =~ s/\s+,/,/g;
+    &print_debug_message('file_info_str', 'End', 11);
     return $file_type_str;
 }
+
+=head2 file_metadata()
+
+Extract metadata from file (e.g. EXIF from JPEGs).
+
+  $metadata_hash = file_metadata($filename);
+
+Uses L<Image::ExifTool> to fetch metadata.
+
+=cut
+
+sub file_metadata {
+    &print_debug_message('file_metadata', 'Begin', 11);
+    my $filename = shift;
+    my $metadata = {};
+    # Create a new Image::ExifTool object
+    my $exifTool = new Image::ExifTool;
+    # Extract meta information from an image
+    $exifTool->ExtractInfo($filename);
+    # Extract common information relating to source.
+    my @tagList = (
+	### EXIF metadata tags (e.g. JPEG and TIFF)
+	'Make',
+	'Model',
+	'Software',
+	'Artist',
+	'Copyright',
+	'UserComment',
+	'FileSource',
+	'SerialNumber',
+	'ImageDescription',
+	### IPTC profile
+	#'Keywords',
+	#'Source',
+	#'CopyrightNotice',
+	#'Caption'
+	### XMP (inc. Dublin Core)
+	'Author',
+	#'Caption',
+	'Keywords',
+	#'Notes',
+	#'OwnerName',
+	#'SerialNumber',
+	#'License',
+	### ICC
+	'ManufacturerName',
+	### Dublin Core (e.g. MS Office documents)
+	'Title',
+	'Creator',
+	'Subject',
+	'Description',
+	#'Publisher',
+	'Contributor',
+	#'Date',
+	#'Type',
+	#'Format',
+	#'Identifier',
+	'Source',
+	'Language',
+	#'Relation',
+	#'Coverage',
+	'Rights',
+	### TODO: Dublin Core Metadata Initiative (DCMI)
+	);
+    foreach my $tag (@tagList) {
+	# Get the value of a specified tag
+	my $value = $exifTool->GetValue($tag);
+	if($value) {
+	    $metadata->{$tag} = $value;
+	}
+    }
+    &print_debug_message('file_metadata', Dumper($metadata), 12);
+    &print_debug_message('file_metadata', 'End', 11);
+    return $metadata;
+}    
 
 =head2 wanted()
 
@@ -443,7 +602,7 @@ sub wanted {
     &print_debug_message('wanted', 'Begin', 11);
     &print_debug_message('wanted', 'File: ' . $_, 12);
     my (%checksums) = ();
-    my ($file_ext, $file_type_str, $mime_type, $stat_data);
+    my ($file_ext, $file_type_str, $mime_type, $stat_data, $metadata);
     # Identify files
     if( ($stat_data = lstat($_)) &&
 	-f $stat_data ) {
@@ -459,8 +618,8 @@ sub wanted {
 	    $mime_type = &file_mime_type($mime_method, $filename, $esc_filename, $rel_filename, $file_utility);
 	    # File information (file utility).
 	    $file_type_str = &file_info_str($esc_filename, $rel_filename, $file_utility);
-	    # [TODO] Extract meta-data from file (e.g. EXIF from JPEGs)
-	    # Image::ExifTool, Image::Magick
+	    # Extract meta-data from file (e.g. EXIF from JPEGs)
+	    $metadata = &file_metadata($filename);
 	}
 	else {
 	    warn "Unable to read $rel_filename, so no digest, MIME or file type recorded";
@@ -480,10 +639,20 @@ sub wanted {
 	}
 
 	# Add to database
-	my (%colValues) = (
+	# 1. Get/add source
+	my $source_id = &add_source($dbh, {
+	    'Make' => $metadata->{'Make'} ||
+		$metadata->{'ManufacturerName'},
+	    'Model' => $metadata->{'Model'},
+	    'Software' => $metadata->{'Software'},
+	    'FileSource' => $metadata->{'FileSource'},
+	    'SerialNumber' => $metadata->{'SerialNumber'},
+				    });
+	# 2. Add file information.
+	my $colValues = {
 	    'fullFilename'  => $rel_filename,
 	    'mtime'         => &frmt_time($stat_data->mtime),
-	    'sizeKb'        => $stat_data->size,
+	    'sizeBytes'     => $stat_data->size,
 	    'sizeBlocks'    => $stat_data->blocks,
 	    'blockSize'     => $stat_data->blksize,
 	    'MD5'           => $checksums{'MD5'},
@@ -493,21 +662,36 @@ sub wanted {
 	    'filename'      => $filename,
 	    'fileExtension' => $file_ext,
 	    'fileType'      => $file_type_str,
-	    );
-	&add_files($dbh, %colValues);
+	    'Author'        => $metadata->{'Creator'} ||
+		$metadata->{'Author'} ||
+		$metadata->{'Artist'} ||
+		$metadata->{'Contributor'},
+	    'Title'         => $metadata->{'Title'} ||
+		$metadata->{'Description'} ||
+		$metadata->{'ImageDescription'},
+	    'Comment'       => $metadata->{'Comment'} ||
+	        $metadata->{'Subject'} ||
+                $metadata->{'Keywords'},
+	    'Copyright'     => $metadata->{'Copyright'} ||
+	        $metadata->{'Rights'},
+	    'sourceId'      => $source_id,
+	    };
+	&add_files($dbh, $colValues);
 	
 	# Print to tab-delimited format.
-	foreach my $checksum_type (sort(keys(%checksums))) {
-	    print $checksums{$checksum_type}, "\t";
-	}
+	#foreach my $checksum_type (sort(keys(%checksums))) {
+	#    print $checksums{$checksum_type}, "\t";
+	#}
 	print(
-	    &frmt_time($stat_data->mtime), 
-	    "\t", $stat_data->size, "\t", $stat_data->blocks, '*', $stat_data->blksize, 
-	    "\t", $mime_type,
-	    "\t", $file_ext,
-	    "\t", $file_type_str,
+	    #&frmt_time($stat_data->mtime), 
+	    #"\t", $stat_data->size, "\t", $stat_data->blocks, '*', $stat_data->blksize, 
+	    #"\t", $mime_type,
+	    #"\t", $file_ext,
+	    #"\t", $file_type_str,
 	    "\t", $rel_filename, 
-	    #"\t", $dirname, "\t", $filename, 
+	    #"\t", $dirname, "\t", $filename,
+	    #"\t", ($metadata->{'Author'} || $metadata->{'Artist'}),
+	    #"\t", ($metadata->{'Title'} || $metadata->{'Description'} || $metadata->{'ImageDescription'}),
 	    "\n");
     }
     &print_debug_message('wanted', 'End', 11);
